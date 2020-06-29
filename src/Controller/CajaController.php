e814cd264ce291f1a0c430fc86cb6a2cfc96b15a
gordomo <morimartin@gmail.com>
2020-06-29 10:24:46 -0300

    pv-03 - primer modulo - caja

---
+++ src/Controller/CajaController.php
@@ -0,0 +1,141 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Caja;
+use App\Form\CajaType;
+use App\Repository\CajaRepository;
+use Knp\Component\Pager\PaginatorInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+
+/**
+ * @Route("/caja")
+ */
+class CajaController extends AbstractController
+{
+    /**
+     * @Route("/", name="caja_index", methods={"GET","POST"})
+     */
+    public function index(CajaRepository $cajaRepository, Request $request, PaginatorInterface $paginator): Response
+    {
+        $caja = new Caja();
+        $caja->setIngreso(0);
+        $caja->setEgreso(0);
+        $caja->setFecha(new \DateTime('now'));
+        $caja->setLlevaTicket(true);
+        $form = $this->createForm(CajaType::class, $caja);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($caja);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('caja_index');
+        }
+
+        $buscar = $request->query->get('buscar') ?? '';
+        $desde = $request->query->get('desde') ?? '';
+        $hasta = $request->query->get('hasta') ?? '';
+
+        if($desde != '') {
+            $desde = new \DateTime($desde);
+            if ($hasta == '') {
+                $hasta = new \DateTime();
+            } else {
+                $hasta = new \DateTime($hasta);
+            }
+        }
+
+        $cajasQuery = $cajaRepository->findByCustom($buscar, $desde, $hasta);
+
+        $pagination = $paginator->paginate(
+            $cajasQuery, /* query NOT result */
+            $request->query->getInt('page', 1), /*page number*/
+            10 /*limit per page*/
+        );
+
+        $desdeDP = ($desde != '') ? $desde->format('d/m/Y') : '';
+        $hastaDP = ($hasta != '') ? $hasta->format('d/m/Y') : '';
+
+        return $this->render('caja/index.html.twig', [
+            'pagination' => $pagination,
+            'caja' => $caja,
+            'form' => $form->createView(),
+            'buscar' => $buscar,
+            'desde' => $desdeDP,
+            'hasta' => $hastaDP,
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="caja_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $caja = new Caja();
+        $form = $this->createForm(CajaType::class, $caja);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($caja);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('caja_index');
+        }
+
+        return $this->render('caja/new.html.twig', [
+            'caja' => $caja,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="caja_show", methods={"GET"})
+     */
+    public function show(Caja $caja): Response
+    {
+        return $this->render('caja/show.html.twig', [
+            'caja' => $caja,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="caja_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Caja $caja): Response
+    {
+        $form = $this->createForm(CajaType::class, $caja);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('caja_index');
+        }
+
+        return $this->render('caja/edit.html.twig', [
+            'caja' => $caja,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="caja_delete", methods={"DELETE"})
+     */
+    public function delete(Request $request, Caja $caja): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$caja->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($caja);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('caja_index');
+    }
+}
---
+++ src/Repository/CajaRepository.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Repository;
+
+use App\Entity\Caja;
+use Doctrine\Bundle\DoctrineBundle\Repository\ServiceEntityRepository;
+use Doctrine\Persistence\ManagerRegistry;
+
+/**
+ * @method Caja|null find($id, $lockMode = null, $lockVersion = null)
+ * @method Caja|null findOneBy(array $criteria, array $orderBy = null)
+ * @method Caja[]    findAll()
+ * @method Caja[]    findBy(array $criteria, array $orderBy = null, $limit = null, $offset = null)
+ */
+class CajaRepository extends ServiceEntityRepository
+{
+    public function __construct(ManagerRegistry $registry)
+    {
+        parent::__construct($registry, Caja::class);
+    }
+
+    // /**
+    //  * @return Caja[] Returns an array of Caja objects
+    //  */
+
+    public function findByExampleField($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ->getResult()
+        ;
+    }
+    public function findByDesc($value)
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.descrip like  :val')
+            ->setParameter('val','%'. $value .'%')
+            ->orderBy('c.id', 'ASC')
+            ->setMaxResults(10)
+            ->getQuery()
+            ;
+    }
+    public function findByCustom($desc = '', $desde = '', $hasta = '')
+    {
+        $query = $this->createQueryBuilder('c');
+        if($desc != '') {
+            $query = $query->andWhere('c.descrip like  :descrip')->setParameter('descrip','%'. $desc .'%');
+        }
+        if($desde != '') {
+
+            $from = new \DateTime($desde->format("Y-m-d")." 00:00:00");
+            $to   = new \DateTime($hasta->format("Y-m-d")." 23:59:59");
+            $query = $query->andWhere('c.fecha BETWEEN  :from AND :to')
+                ->setParameter('from', $from )
+                ->setParameter('to', $to);
+        }
+
+
+        return $query
+            ->orderBy('c.id', 'ASC')
+            ->getQuery();
+    }
+
+    public function buscarTodos()
+    {
+        return $this->getEntityManager()
+            ->createQuery('
+                SELECT c.id, c.ingreso, c.egreso, c.llevaTicket, c.fecha, c.descrip FROM App:Caja as c
+            ');
+    }
+
+    /*
+    public function findOneBySomeField($value): ?Caja
+    {
+        return $this->createQueryBuilder('c')
+            ->andWhere('c.exampleField = :val')
+            ->setParameter('val', $value)
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+    */
+}
